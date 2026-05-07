9e58c51ffc4541fbc04a910040e2385fd9f540b2
Millevanille <mille.wikstrom10@gmail.com>
2026-05-07 16:22:13 +0200

    Klar med dollar-programmet och tja.py

---
+++ Tja.py
@@ -0,0 +1,6 @@
+namn = input("Skriv in vallfrit namn: ")
+djur = input("Skriv in valfritt djur: ")
+färg = input("Skriv in valfri färg: ")
+
+print(f"Det va en gång en {djur} som hette {namn}")
+print(f"Den var helt {färg} och glänste")
