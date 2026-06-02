cf9ada2f86caca2bbdfc92ea59af9920043cbdea
Millevanille <mille.wikstrom10@gmail.com>
2026-06-02 20:34:44 +0200

    Fixade filnamnet

---
+++ Quiz.py
@@ -0,0 +1,23 @@
+y = input("Är du reda för de enklaste quizet någonsin?")
+if y.lower() == "ja":   print("Jag hoppas de")
+
+else: print("Jaha de var väll synd för nu kör vi!")
+
+#Fråga 1
+x = int(input("Vad är 25 dividerat med 5?"))
+if x==5:    print("Bibibibibi bingo")  
+else:   print("FELLLL svaret är fem")
+
+#Fårga 2
+
+print("Vilken är den 'smartaste' kontinenten? ")
+print("A Europa")
+print("B Asien")
+print("C Antarktis")
+print("D Australien")
+
+x = input()
+if x.lower() == "c": print("Japp de där är korrekt")
+else: print("Nope C, Antarktis")
+
+#Fråga 3
--- Sek_till_Dollar.py
+++ Sek_till_Dollar.py
@@ -2,4 +2,4 @@ sek_text = input("Hur mycket pengar har du?")
 
 sek_nummer = int(sek_text)
 dollar = sek_nummer/10.5
-print(f"Dina {sek_nummer} blir {dollar} dollar")
+print(f"Dina {sek_nummer} blir {dollar} dollar"
